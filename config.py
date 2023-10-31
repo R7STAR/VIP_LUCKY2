4a717073b5a1644e98a8c253661927aca2e2b106
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2023-10-31 22:32:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+    "https://github.com/L2LUCKY/Vip_Lucky",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
