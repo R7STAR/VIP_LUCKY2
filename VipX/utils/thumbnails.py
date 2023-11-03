aeabd996b8d5835a85d758229a6d8be75455153b
R7STAR <141929043+R7STAR@users.noreply.github.com>
2023-11-03 22:51:45 +0530

    Update thumbnails.py

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -82,7 +82,7 @@ async def gen_thumb(videoid, user_id):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"VipX/assets/anonx.png")
+        bg = Image.open(f"VipX/assets/anonx/anonx3.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
@@ -120,12 +120,12 @@ async def gen_thumb(videoid, user_id):
         font = ImageFont.truetype("VipX/assets/font2.ttf", 45)
         ImageFont.truetype("VipX/assets/font2.ttf", 70)
         arial = ImageFont.truetype("VipX/assets/font2.ttf", 30)
-        ImageFont.truetype("VipX/assets/font.ttf", 30)
+        ImageFont.truetype("VipX/assets/font.otf", 30)
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
                 (450, 25),
-                f"LUCKY MUSIC",
+                f"LUCKY MUSIC PLAYING",
                 fill="white",
                 stroke_width=3,
                 stroke_fill="grey",
@@ -222,7 +222,7 @@ async def gen_qthumb(videoid, user_id):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"VipX/assets/anonx.png")
+        bg = Image.open(f"VipX/assets/anonx/anonx3.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
@@ -260,7 +260,7 @@ async def gen_qthumb(videoid, user_id):
         font = ImageFont.truetype("VipX/assets/font2.ttf", 45)
         ImageFont.truetype("VipX/assets/font2.ttf", 70)
         arial = ImageFont.truetype("VipX/assets/font2.ttf", 30)
-        ImageFont.truetype("VipX/assets/font.ttf", 30)
+        ImageFont.truetype("VipX/assets/font.otf", 30)
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
