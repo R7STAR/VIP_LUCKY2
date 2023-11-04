f9ecbba9978940ff26bd9d024d349dc22e12c1fe
R7STAR <141929043+R7STAR@users.noreply.github.com>
2023-11-04 11:02:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/R7STAR/VIP_LUCKY",
+    "https://github.com/R7STAR/VIP_LUCKY2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
